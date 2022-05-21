a5436bf8fba82c4040015dfc32e0f85cf7687291
Kaique Silva <kaique_gp@hotmail.com>
2022-05-21 13:24:04 -0300

    teste

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+function exibirMenu(){
+    document.getElementById("conteudo-menu").style.display="flex";
+}
+function ocutarMenu(){
+    document.getElementById("conteudo-menu").style.display="none";
+}
