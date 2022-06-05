803b5e8ccdb7cc32d9355301e625434505a82969
Kaique Silva <kaique_gp@hotmail.com>
2022-06-04 22:23:56 -0300

    Validação de formulário

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,17 +1,14 @@
-function testaFormulario(e){
+function testaFormulario(e) {
     // e.preventDefault();
-
-    var Mercadoria = /[^A-z ]+/g
-    var Valor = /[^0-1]/g
-    var nome = document.getElementById("nm-merca");
-    if(Mercadoria.test(nome)){
+    var Valor = /[^0-9]+/g;
+    var Mercadoria = /[^A-z]+/g;
+    var numValor = document.getElementById("valor-input").value
+    var nome = document.getElementById("nm-merca").value
+    if (Mercadoria.test(nome)) {
         alert('Este não é um nome válido!')
-    } 
+    };
 
-    if (Valor.test(Valor)){
-        alert('Valor inválido!')
+    if (Valor.test(numValor)){
+        alert ('Este Valor é inválido!')
     }
-
-
-
 }
