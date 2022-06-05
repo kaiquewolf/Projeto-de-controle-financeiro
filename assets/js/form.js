4812c0cbeadac8011bdebff032cc529dce95c440
Kaique Silva <kaique_gp@hotmail.com>
2022-06-05 11:47:01 -0300

    Feito mascara nos campos para validação

--- assets/js/form.js
+++ assets/js/form.js
@@ -12,3 +12,20 @@ function testaFormulario(e) {
         alert ('Este Valor é inválido!')
     }
 }
+
+function testaCampo(t){
+    var Valor = /[^0-9]+/g;
+    var Mercadoria = /[^A-z]+/g;
+    var numValor = document.getElementById("valor-input").value
+    var nome = document.getElementById("nm-merca").value
+    if (Mercadoria.test(nome)) {
+        alert('Este não é um nome válido!')
+        document.getElementById("nm-merca").focus();
+    } 
+
+
+    if (Valor.test(numValor)){
+        alert ('Este Valor é inválido!')
+        document.getElementById("valor-input").focus();
+    }
+}
